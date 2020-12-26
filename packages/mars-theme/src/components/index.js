794361dc0ce0cca139392f7278b5684fa9f28a0e
Tomasz Sypko <tsypko@gmail.com>
2020-12-26 20:32:54 +0100

    Hero changed to MainContainer, changed Hero to fixed background

--- packages/mars-theme/src/components/index.js
+++ packages/mars-theme/src/components/index.js
@@ -10,48 +10,32 @@ import PageError from "./page-error";
 import Hero from "./hero";
 import heroImage from "../assets/heroImage.jpg";
 
-/**
- * Theme is the root React component of our theme. The one we will export
- * in roots.
- */
 const Theme = ({ state }) => {
-  // Get information about the current URL.
   const data = state.source.get(state.router.link);
 
   return (
     <>
-      {/* Add some metatags to the <head> of the HTML. */}
       <Title />
       <Head>
         <meta name="description" content={state.frontity.description} />
         <html lang="en" />
-        <link rel="preconnect" href="https://fonts.gstatic.com"/>
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet" />
       </Head>
-
-      {/* Add some global styles for the whole site, like body or a's. 
-      Not classes here because we use CSS-in-JS. Only global HTML tags. */}
       <Global styles={globalStyles} />
-
-      {/* Add the header of the site. */}
       <HeadContainer>
         <Header />
       </HeadContainer>
-
-      {/* Add the main section. It renders a different component depending
-      on the type of URL we are in. */}
-      <HeroContainer>
-        <Hero />
-      </HeroContainer>
-      <Main>
-
-        <Switch>
-          <Loading when={data.isFetching} />
-          <List when={data.isArchive} />
-          <Post when={data.isPostType} />
-          <PageError when={data.isError} />
-        </Switch>
-      </Main>
+      <MainContainer>
+        <Main>
+          <Switch>
+            <Loading when={data.isFetching} />
+            <List when={data.isArchive} />
+            <Post when={data.isPostType} />
+            <PageError when={data.isError} />
+          </Switch>
+        </Main>
+      </MainContainer>
     </>
   );
 };
@@ -72,28 +56,33 @@ const globalStyles = css`
 `;
 
 const HeadContainer = styled.div`
+  position: sticky;
+  top: 0px;
   display: flex;
   align-items: center;
   flex-direction: column;
   background-color: #000;
+  box-shadow: 0px 0px 7px 5px rgba(0,0,0);
 `;
 
-const HeroContainer = styled.section`
+const MainContainer = styled.section`
     width: 100%;
-    height: 50vh;
+    height: max-content;
     background: url(${heroImage});
-    background-position: center;
+    background-attachment: fixed;
     background-size: cover;
     background-repeat: no-repeat;
+    background-position: center center;
 `;
 
-const Main = styled.div`
+const Main = styled.main`
   display: flex;
   justify-content: center;
-  background-image: linear-gradient(
-    180deg,
-    rgba(66, 174, 228, 0.1),
-    rgba(66, 174, 228, 0)
-  );
-  box-shadow: inset 0px 3px 7px 0px rgba(0,0,0,0.75);
+  background: 
+  linear-gradient(0deg,
+    rgba(0,0,0,1) 0%,
+    rgba(56,253,255,0) 25%,
+    rgba(56,253,255,0) 75%,
+    rgba(0,0,0,1) 100%);
+  box-shadow: inset 0px -5px 7px 0px rgba(0,0,0);
 `;
