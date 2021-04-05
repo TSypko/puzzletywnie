fad29fa39d0eadd38e1f1cf47ce280bbc76b9b9c
Tomasz Sypko <tsypko@gmail.com>
2021-04-05 11:42:10 +0200

    padding added

--- packages/mars-theme/src/components/tile.js
+++ packages/mars-theme/src/components/tile.js
@@ -4,6 +4,7 @@ const Tile = styled.article`
   background-color: #ffffff;
   box-shadow: 0px 0px 11px 2px rgba(0,0,0,0.35);
   margin-bottom: 36px;
+  padding: 8px;
 
   ${({ post }) => post && css`
           padding: 24px
