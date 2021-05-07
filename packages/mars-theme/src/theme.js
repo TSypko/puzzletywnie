5ef2589bcba3ce1a3130d551813433a889e26410
Tomasz Sypko <tsypko@gmail.com>
2021-05-07 23:31:17 +0200

    theme updated

--- packages/mars-theme/src/theme.js
+++ packages/mars-theme/src/theme.js
@@ -1,12 +1,16 @@
 export const theme = {
     colors: {
       White: "#FFFFFF",
+      Matisse: "#1C7BB0",
+      Blumine: "#176087",
       Haiti: "#0B102A",
       Black: "#000000",
     },
   
-    breakpoints: {
-      mobileMax: "568px",
-      headerMax: "767px"
-    },
+    transitions: {
+        slow: "0.6s",
+        medium: "0.4s",
+        fast: "0.2s",
+        none: "0s"
+      },
   };
