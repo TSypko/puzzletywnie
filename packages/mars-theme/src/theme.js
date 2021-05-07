1fb8b7314201ff9d7e9b3a74da2ab52912218bd6
Tomasz Sypko <tsypko@gmail.com>
2021-05-07 23:02:27 +0200

    theme file added

---
+++ packages/mars-theme/src/theme.js
@@ -0,0 +1,12 @@
+export const theme = {
+    colors: {
+      White: "#FFFFFF",
+      Haiti: "#0B102A",
+      Black: "#000000",
+    },
+  
+    breakpoints: {
+      mobileMax: "568px",
+      headerMax: "767px"
+    },
+  };
